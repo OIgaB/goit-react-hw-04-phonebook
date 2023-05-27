1ddd8a91e7760ee579d944fbed410c9c5e377726
OIgaB <linkbolya@gmail.com>
2023-05-27 04:05:44 +0300

    code refactoring with the useState hook in ContactForm

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,66 +1,64 @@
 // Форма додавання контактів. Це статична форма - не змінна (при відправці викликає ф-цію зміни state)
 
-import { Component } from "react";                     // для класів
+import { useState } from "react";                     
 import PropTypes from 'prop-types';
 import { Form, Input, Button } from "./styled";
 
 
-export class ContactForm extends Component {       // для класів
+export const ContactForm = ({ addContact }) => {       
     // state потрібен для подальшої очистки форми (reset)
-    state = { 
-        name: '',
-        number: '',
-    }
+    const [name, setName] = useState('');
+    const [number, setNumber] = useState('');
     
+
     //Функція записує нові значення в state
-    handleChange = (event) => {
-        this.setState({ [event.target.name]: event.target.value }); // name - це значення атрибута <input> (name i number)     
+    //this.setState({ [event.target.name]: event.target.value }); // name - це значення атрибута <input> (name i number)
+    const handleChangeName = (event) => {
+        setName(event.target.value);     
     }
 
-    handleFormSubmit = (event) => {
+    const handleChangeNumber = (event) => {
+        setNumber(event.target.value);
+    }
+
+    const handleFormSubmit = (event) => {
         event.preventDefault();
-        this.props.addContact(this.state); // передає об'єкт 1го контакта {name: _, number: _} до App.jsx 
-        this.setState({ 
-            name: '',
-            number: '',
-        });  
+        addContact({ name, number }); // передає об'єкт 1го контакта {name: '_', number: '_'} до App.jsx 
+        setName('');
+        setNumber('');
     }
 
-    render() {
-        const { handleFormSubmit, handleChange } = this;
-        const { name, number } = this.state;
-        return (
-            <Form onSubmit={handleFormSubmit} autoComplete="off">
-                <label>
-                    Name
-                        <Input
-                            type="text"
-                            name="name"
-                            value={name} // контрольований input (без цього reset не зробиш)
-                            pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-                            title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-                            placeholder="Oles Honchar"
-                            required
-                            onChange={handleChange}
-                        />
-                </label>
-                <label>
-                    Number
+    return (
+        <Form onSubmit={handleFormSubmit} autoComplete="off">
+            <label>
+                Name
                     <Input
-                        type="tel"
-                        name="number"
-                        value={number} // контрольований input (без цього reset не зробиш)
-                        pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
-                        title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+                        type="text"
+                        name="name"
+                        value={name} // контрольований input (без цього reset не зробиш)
+                        pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+                        title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+                        placeholder="Oles Honchar"
                         required
-                        placeholder="459-12-56"
-                        onChange={handleChange}
+                        onChange={handleChangeName}
                     />
-                </label>
-                <Button>Add contact</Button>
-            </Form>
-        );
-    }
+            </label>
+            <label>
+                Number
+                <Input
+                    type="tel"
+                    name="number"
+                    value={number} // контрольований input (без цього reset не зробиш)
+                    pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
+                    title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
+                    required
+                    placeholder="459-12-56"
+                    onChange={handleChangeNumber}
+                />
+            </label>
+            <Button>Add contact</Button>
+        </Form>
+    );
 }
 
 ContactForm.propTypes = {
